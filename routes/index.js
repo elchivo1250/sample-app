4ec2f18a2a54fa1f994675258dc51f9f4d82a26f
Steve Preston <spreston@boydcatongroup.com>
2016-02-25 00:39:08 -0500

    Fixed routing to remove weird passport error

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,47 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
-/* GET home page. */
-// next removed
 router.get('/', function routeHomepage(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    message: req.flash('message')
+  });
 });
+
+router.get('/survey', isLoggedIn, function (req, res) {
+  res.render('survey', { 
+    message: req.flash('message'),
+    user: req.user
+  });
+});
+
+router.post('/login', passport.authenticate('login', {
+  successRedirect: '/',
+  failureRedirect: '/',
+  failureFlash: true
+}));
+
+router.get('/register', function (req, res) {
+  res.render('register', { 
+    message: req.flash('message'),
+    user: req.user
+
+  });
+});
+
+router.post('register', passport.authenticate('register', {
+  successRedirect: '/',
+  failureRedirect: '/register',
+  failureFlash: true
+}));
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+
+  res.redirect('/');
+}
 
 module.exports = router;
