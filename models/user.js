23bc8129bd058315b2eec51d2c68ba56259b4c49
Steve Preston <spreston@boydcatongroup.com>
2016-02-25 00:38:35 -0500

    Fixed user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var passportLocalSequelize = require('passport-local-sequelize');
+
 module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
