16cbba548f443252c70422b6e730a3382af4d6de
Steve Preston <spreston@boydcatongroup.com>
2016-02-29 12:26:49 -0500

    Finished routing

--- routes/base.js
+++ routes/base.js
@@ -2,24 +2,88 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var models = require('../models');
+var acl = require('../acl');
+var db = require('../models/index');
 
 router.get('/', function (req, res) {
-  typeof(user) === 'undefined'
   res.render('index', {
-    message: req.flash('message'),
-    user: req.user,
-    isLoggedIn: req.user ? true : false
-  });
-});
-
-router.get('/survey', passport.authenticate('local', {
-  failureRedirect: '/login',
-  failureFlash: 'You must be logged in to view this page' 
-}), function (req, res) {
-  res.render('survey', { 
     message: req.flash('message'),
     user: req.user
   });
+});
+
+router.get('/survey', acl.isLoggedIn, function (req, res) {
+    
+  var query = 'SELECT DISTINCT Questions.id FROM Questions LEFT JOIN Answers ON Answers.QuestionId = Questions.id WHERE Questions.id NOT IN (SELECT Questions.id FROM Questions LEFT JOIN Answers ON Answers.QuestionId = Questions.id LEFT JOIN UserAnswers ON UserAnswers.AnswerId = Answers.id WHERE UserAnswers.UserId = :userId)';
+
+  db.sequelize.query(query, { 
+    replacements: { 
+      userId: req.user.id 
+    }, 
+    type: db.Sequelize.QueryTypes.SELECT 
+  }).then(function (questions) {
+
+    if (typeof questions === 'undefined' || questions.length === 0) {
+      throw new Error('There are no more questions'); 
+    }
+
+    // Get random question first because if you include: [models.Answer] it wants to get the 
+    // ANSWERS in a random order and limit the potential questions to 1. ._.
+    // Please tell me there's a better way to do this.
+    models.Question.find({
+      where: {
+        $or: questions
+      },
+      order: [
+        db.Sequelize.fn('RAND')
+      ]
+    }).then(function (question) {
+      models.Answer.findAll({
+        where: {
+          QuestionId: question.id
+        },
+        include: [models.Question]
+      }).then(function (answers) {
+        res.render('survey', {
+          message: req.flash('message'),
+          question: answers[0].Question,
+          answers: answers,
+          user: req.user
+        });
+      }).catch(function (err) {
+        res.render('survey', {
+          message: req.flash('message'),
+          user: req.user
+        });
+      });
+    }).catch(function (err) {
+      res.render('survey', {
+        message: req.flash('message'),
+        user: req.user
+      });
+    });
+  }).catch(function (err) {
+    res.render('survey', {
+      message: req.flash('message'),
+      user: req.user
+    });
+  });
+});
+
+router.post('/survey/answer', function (req, res) {
+  models.UserAnswer.build({
+    UserId: req.user.id,
+    AnswerId: req.body['answer']
+  })
+  .save()
+  .then(function () {
+    console.log('success');
+  })
+  .catch(function (err) {
+    // insert other error handling here
+    console.log('error:' + err);
+  });
+  res.redirect('/survey');
 });
 
 module.exports = router;
